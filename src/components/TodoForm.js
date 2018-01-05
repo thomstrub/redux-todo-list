5dac811ea5a2378dd96785195e931f7d74c95fea
king0120 <king0120@gmail.com>
2018-01-05 12:11:43 -0500

    local state in multiple places

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -1,10 +1,22 @@
 import React, { Component } from 'react'
 
 class TodoForm extends Component {
+  state = {
+    newTodo: ''
+  }
+
+  handleChange = (event) => {
+    console.log(event.target.value)
+    this.setState({ newTodo: event.target.value })
+  }
+
   render () {
     return (
       <div>
-        <input />
+        <input
+          onChange={this.handleChange}
+          value={this.state.newTodo}
+        />
         <button>Add Todo</button>
       </div>
     )
