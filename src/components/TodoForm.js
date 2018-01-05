0ddaee2c6680a2c86b4a80654d38741870b34d0f
king0120 <king0120@gmail.com>
2018-01-05 11:24:08 -0500

    basic react app

---
+++ src/components/TodoForm.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class TodoForm extends Component {
+  render () {
+    return (
+      <div>
+        TODOSFORM
+      </div>
+    )
+  }
+}
+
+export default TodoForm
---
+++ src/components/Todos.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class Todos extends Component {
+  render () {
+    return (
+      <div>
+        TODOS
+      </div>
+    )
+  }
+}
+
+export default Todos
