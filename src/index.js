26d0645d4606055039878cbc60655079ad87d8a4
king0120 <king0120@gmail.com>
2018-01-10 19:51:16 -0500

    configured starter-code

--- src/index.js
+++ src/index.js
@@ -1,16 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { Provider } from 'react-redux'
-import configureStore from './configureStore'
 import App from './components/App'
 import registerServiceWorker from './registerServiceWorker'
 
-// const store = configureStore()
-
-const Root = () => (
-  // <Provider store={store}>
-    <App />
-  // </Provider>
-)
 ReactDOM.render(<App />, document.getElementById('root'))
 registerServiceWorker()
