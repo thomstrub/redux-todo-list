f7b1d091098c28bc3f6837022d6bd5211f06870e
Thomas Strub <thomstrub@gmail.com>
2021-03-02 21:17:34 -0800

    adds the provider component

--- src/index.js
+++ src/index.js
@@ -2,6 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './components/App'
 import registerServiceWorker from './registerServiceWorker'
+import { Provider } from 'react-redux'
+import configureStore from './configureStore'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+const store = configureStore();
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>
+, document.getElementById('root')
+)
+
 registerServiceWorker()
